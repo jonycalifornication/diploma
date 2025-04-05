35fd30ab32cfbfa7f44fffe2c32672ed31e4253f
jonycalifornication <khaypkhan10@gmail.com>
2025-04-05 15:20:34 +0500

    initial commit

--- handlers/states.py
+++
@@ -1,4 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class Request(StatesGroup):
-    write_request = State()
--- keyboards/start_keyboard.py
+++
@@ -1,7 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-start_keyboard = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [InlineKeyboardButton(text="Write a request", callback_data="write_request")]
-    ]
-)
